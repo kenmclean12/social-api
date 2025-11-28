032bd2d0c9f5431ac7d882e6c67b71156e1d7a60
kenmclean12 <kenmclean20@gmail.com>
2025-11-28 13:17:48 -0800

    cleanup

--- src/follow/follow.module.ts
+++ src/follow/follow.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { FollowController } from './follow.controller';
 import { FollowService } from './follow.service';
+import { Follow } from './entities/follow.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forFeature([Follow])],
   controllers: [FollowController],
   providers: [FollowService],
   exports: [FollowService],
--- src/post/post.module.ts
+++ src/post/post.module.ts
@@ -1,4 +1,12 @@
-import { Module } from '@nestjs/common';
+import { Module, Post } from '@nestjs/common';
+import { PostController } from './post.controller';
+import { PostService } from './post.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
-@Module({})
+@Module({
+  imports: [TypeOrmModule.forFeature([Post])],
+  controllers: [PostController],
+  providers: [PostService],
+  exports: [PostService],
+})
 export class PostModule {}
