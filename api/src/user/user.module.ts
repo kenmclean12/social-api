ea1a7961c3fe4f18188c68235e085b8c2c4ae0f1
kenmclean12 <kenmclean20@gmail.com>
2025-11-29 08:21:31 -0800

    1

--- api/src/user/user.module.ts
+++ api/src/user/user.module.ts
@@ -3,15 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { User } from './entities/user.entity';
-import { Message } from 'src/message/entities/message.entity';
 import { FollowModule } from 'src/follow/follow.module';
 
 @Module({
-  imports: [
-    TypeOrmModule.forFeature([User]),
-    TypeOrmModule.forFeature([Message]),
-    forwardRef(() => FollowModule),
-  ],
+  imports: [TypeOrmModule.forFeature([User]), forwardRef(() => FollowModule)],
   controllers: [UserController],
   providers: [UserService],
   exports: [UserService],
