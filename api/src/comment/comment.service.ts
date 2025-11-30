d92fb323831b28af3fe4cb8e7c486ca121843a6e
kenmclean12 <kenmclean20@gmail.com>
2025-11-30 11:50:41 -0800

    1

--- api/src/comment/comment.service.ts
+++ api/src/comment/comment.service.ts
@@ -64,7 +64,7 @@ export class CommentService {
 
     if (user.id !== comment.user.id) {
       throw new UnauthorizedException(
-        `Only the author of a comment can update the record from the database`,
+        'Only the author of a comment can update it',
       );
     }
 
@@ -78,7 +78,7 @@ export class CommentService {
 
     if (user.id !== comment.user.id) {
       throw new UnauthorizedException(
-        `Only the author of a comment can remove the record from the database`,
+        `Only the author of a comment can remove it`,
       );
     }
 
