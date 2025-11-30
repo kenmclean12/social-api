e78a5be31703613d4713a4d73299b6d540a67c84
kenmclean12 <kenmclean20@gmail.com>
2025-11-30 09:58:58 -0800

    1

--- api/src/post/post.module.ts
+++ api/src/post/post.module.ts
@@ -1,13 +1,14 @@
-import { Module, Post } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { PostController } from './post.controller';
 import { PostService } from './post.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from 'src/user/entities/user.entity';
 import { UserModule } from 'src/user/user.module';
+import { UserPost } from './entities/user-post.entity';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([Post]),
+    TypeOrmModule.forFeature([UserPost]),
     TypeOrmModule.forFeature([User]),
     UserModule,
   ],
