7db4706ae4da7b79fd6a7b74e2b2584259bc9ac2
kenmclean12 <kenmclean20@gmail.com>
2025-12-02 13:50:37 -0800

    fixed notification gateway

--- api/src/notification/notification.gateway.ts
+++ api/src/notification/notification.gateway.ts
@@ -6,6 +6,14 @@ import { WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
 export class NotificationsGateway {
   @WebSocketServer() server;
 
+  handleConnection(client: any) {
+    const userId = client.handshake.auth.userId;
+    if (!userId) return;
+
+    client.join(`user_${userId}`);
+    console.log(`User ${userId} joined room user_${userId}`);
+  }
+
   sendNotification(userId: number, notification: any) {
     this.server.to(`user_${userId}`).emit('notification', notification);
   }
