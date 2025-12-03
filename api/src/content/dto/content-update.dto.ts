8cd12dcdbe044330b8303511e051c9f11a99d132
kenmclean12 <kenmclean20@gmail.com>
2025-12-03 08:15:04 -0800

    removed content update dto

--- api/src/content/dto/content-update.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { ContentCreateDto } from './content-create.dto';
-
-export class ContentUpdateDto extends PartialType(ContentCreateDto) {}
--- api/src/content/dto/index.ts
+++ api/src/content/dto/index.ts
@@ -1,3 +1,2 @@
 export { ContentCreateDto } from './content-create.dto';
-export { ContentUpdateDto } from './content-update.dto';
 export { ContentResponseDto } from './content-response.dto';
